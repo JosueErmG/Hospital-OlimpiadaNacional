73633c2e14d2ccd2453421877ee24148ff4d141b
JosueErmG <144946222+JosueErmG@users.noreply.github.com>
2023-09-14 22:58:50 -0300

    Formatting

--- Sistema/options/2 opt/styles.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver(entries => {
-        document.querySelector("#bar").classList.toggle("bar_bg", entries[0].intersectionRatio < 0.85)
-    }, { threshold: 0.85 });
-
-    observer.observe(document.querySelector(".header"));
-});
-
-
