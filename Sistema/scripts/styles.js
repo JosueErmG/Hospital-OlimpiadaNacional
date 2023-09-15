415a34c8d73cc2caf0580b94c9cbf77d04315b68
JosueErmG <144946222+JosueErmG@users.noreply.github.com>
2023-09-14 21:08:41 -0300

    c

--- Sistema/scripts/styles.js
+++ Sistema/scripts/styles.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver(entries => {
-        document.querySelector("header").classList.toggle("header_bg", entries[0].intersectionRatio < 0.85)
-    }, { threshold: 0.85 });
-
-    observer.observe(document.querySelector("main"));
-});
-
 
 
 
